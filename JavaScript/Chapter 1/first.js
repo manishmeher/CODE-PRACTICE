750f77fc3d64340ac64da15cfaebe9c42a4c41eb
manishmeher <manish.meher774623@gmail.com>
2024-11-06 17:02:57 +0530

    Add JavaScript

---
+++ JavaScript/Chapter 1/first.js
@@ -0,0 +1,27 @@
+const SAMSUNG = {
+    fullname : "SAMSUNG Galaxy S24 FE 5G (Mint, 256 GB) (8 GB RAM)",
+    ratings : 4.5,
+    color: "Mint",
+    storage: "256 GB",
+    price: "65,999",
+};
+console.log(SAMSUNG);
+
+const APPLE = {
+    fullname: "Apple iPhone SE (White, 64 GB)",
+    rating: 4.5,
+    color: "White",
+    storage: "64 GB",
+    price: "39,900",
+};
+console.log(APPLE);
+
+const profile = {
+    nusername: "@manishmeher_",
+    followers: 306,
+    following: 880,
+    post: 34,
+    isfollow: true,
+    bio: "RadhaKrishna",
+}
+console.log(profile);
