7a936be805742def789860a833a19eb914688ee0
manishmeher <manish.meher774623@gmail.com>
2024-12-18 11:47:10 +0530

    Update JS

---
+++ JavaScript/Chapter 5/script.js
@@ -0,0 +1,142 @@
+// function myFunction() {
+//     console.log("Hello, Im Manish Kumar Meher");
+//     console.log("I Love JS");
+// }
+// myFunction()
+
+// function myFunction(msg) {
+//     console.log(msg);
+// }
+// myFunction("This is my function");
+// myFunction("hello");
+
+// function myFunction(x, y) {
+//     console.log(x + y);
+// }
+// myFunction(4, 8);
+
+// function myFunction(x, y) {
+//     return x + y;
+// }
+// let sum = myFunction(4, 8);
+// console.log(sum);
+
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// let val = sum(5, 5);
+
+// let sum = (a, b) => {
+//     return a + b;
+// }
+
+// let sum = (a, b) => {
+//     console.log(a + b);
+// }
+
+// function countVowels(str) {
+//     let count = 0
+//     console.log(str)
+//     for (const char of str) {
+//         if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") 
+//             count++
+//     }
+//     return count;
+// }
+
+// let val = countVowels("manishKumarmeher");
+// console.log(val);
+
+// function countVowels(str) {
+//     let count = 0
+//     console.log(str);
+//     for (const char of str) {
+        // if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+        // count++
+//     }
+//     return count;
+// }
+
+// let val = countVowels("Manish Kumar Meher");
+// console.log(val);
+
+// const countVow = ((str) => {
+//     let count = 0;
+//     for (const char of str) {
+//         if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//                 count++
+//         }
+//     }
+//     console.log(count);
+// })
+
+// let arr = [1, 2, 3, 4];
+// arr.forEach(function printVal(val) {
+//         console.log(val);
+// })
+
+// let arr = [1, 2, 3, 4];
+// arr.forEach((val)=> {
+//         console.log(val);
+// })
+
+// let nums = [1, 2, 3, 4,];
+// nums.forEach((val)=> {
+//         console.log(val * val);
+// })
+
+// let arr = [5, 8, 10, 9];
+// arr.forEach((val)=> {
+//         console.log(val * val);
+// })
+
+// let arr = [1, 2, 3, 4];
+// let newArr = arr.map((val)=> {
+//         return val * val
+// }) 
+// console.log(newArr);
+
+// let arr = [1, 2, 3, 4];
+// let newArr = arr.filter((val)=> {
+//         return val % 2 === 0;
+// })
+// console.log(newArr);
+
+// let arr = [1, 2, 3, 4];
+// const output = arr.reduce((prev, curr)=> {
+//         return prev + curr 
+// })
+// console.log(output);
+
+// let arr = [1, 2, 3, 4];
+// const output = arr.reduce((prev, curr)=> {
+//         return prev > curr ? prev: curr
+// })
+// console.log(output);
+
+// let arr = [56, 94, 98, 99, 33, 46];
+// let newArr = arr.filter((val)=>{
+//         return val > 90;
+// })
+
+// console.log(newArr);
+
+// let user = prompt("enter a number");
+// let arr = [];
+// for(i = 1; i <= user; i++) {
+//         arr[i-1] = i;
+// }
+// console.log(arr);
+// let sum = arr.reduce((prev, curr)=>{
+//         return prev + curr;
+// })
+// console.log(sum);
+
+// let factorial = arr.reduce((prev, curr)=>{
+//         return prev * curr;
+// })
+// console.log(factorial);
+
+
+
+
