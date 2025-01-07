b9d7f9008c2e4bd7e667f73e24efe54adc32ac36
manishmeher <manish.meher774623@gmail.com>
2025-01-07 16:58:12 +0530

    Add Chapter 10

---
+++ JavaScript/Chapter 10/app.js
@@ -0,0 +1,16 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+
+
+//Async-await
+const getFacts = async () => {
+    console.log("getting data...");
+    let response = await fetch(URL);
+    console.log(response);
+    let data = await response.json();
+    factPara.innerText = data[2].text;
+}
+
+btn.addEventListener("click", getFacts);
